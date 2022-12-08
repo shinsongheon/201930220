f9d9348853f1e952dbc676ce6ecab628906623bb
songheon <songheon2000@naver.com>
2022-12-08 14:39:05 +0900

    사용자 환영 UI 및 입력 기능 추가

--- js/example.js
+++ js/example.js
@@ -1,3 +1,21 @@
-const test = 10;
+// 무조건 실행
+const nameH1element = document.querySelector('h1.inline');
+const connectNameElement = document.querySelector('span.red');
+const localName = localStorage.getItem('name');
+if (localName) {
+  nameH1element.textContent = localName;
+  connectNameElement.textContent = localName;
+}
 
-console.log(test);
+// 클릭 했을때만 발생
+nameH1element.onclick = () => {
+  const inputName = prompt('이름을 입력해 주세요.');
+  if (inputName) {
+    localStorage.setItem('name', inputName);
+
+    nameH1element.textContent = inputName;
+    connectNameElement.textContent = inputName;
+  } else {
+    alert('이름이 입력되지 않았습니다.');
+  }
+};
